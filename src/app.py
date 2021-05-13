02b1e15fb655d7b08129a0bee426f3285c8ec5b1
Yahya Ghani <yambox88@gmail.com>
2021-05-13 13:00:28 +0100

    back

--- src/app.py
+++ src/app.py
@@ -2,39 +2,20 @@
 
 # load libaries
 from flask import Flask, jsonify
+from flask_cors import CORS
 import sys
 
 # load modules
-from src.endpoints.blueprint_x import blueprint_x
-from src.endpoints.blueprint_y import blueprint_y
-from src.endpoints.swagger import swagger_ui_blueprint, SWAGGER_URL
+from src.endpoints.blueprint_api import bp_api
 
 # init Flask app
 app = Flask(__name__)
+CORS(app) # This will enable CORS for all routes
+app.config['CORS_HEADERS'] = 'application/json'
 
 # register blueprints. ensure that all paths are versioned!
-app.register_blueprint(blueprint_x, url_prefix="/api/v1/path_for_blueprint_x")
-app.register_blueprint(blueprint_y, url_prefix="/api/v1/path_for_blueprint_y")
+app.register_blueprint(bp_api, url_prefix="/api/v1/")
 
-from src.api_spec import spec
-# register all swagger documented functions here
-
-with app.test_request_context():
-    for fn_name in app.view_functions:
-        if fn_name == 'static':
-            continue
-        print(f"Loading swagger docs for function: {fn_name}")
-        view_fn = app.view_functions[fn_name]
-        spec.path(view=view_fn)
-
-@app.route("/api/swagger.json")
-def create_swagger_spec():
-    """
-    Swagger API definition.
-    """
-    return jsonify(spec.to_dict())
-
-app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
