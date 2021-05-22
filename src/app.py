d00c82990ebf8f2a3e691455188da5477f77c992
Yahya Ghani <yambox88@gmail.com>
2021-05-21 22:49:20 -0400

    Remove unused packages

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify
-from src.routes.blueprint_api import bp_api
+from flask import Flask
+from src.routes.pdf_api import bp_api
 from flask_socketio import SocketIO, emit
 from engineio.payload import Payload
 from flask_cors import CORS
